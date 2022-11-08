db7df19ae50de8d06e33060190a38ca7fc88791e
Evgeniy Spevak <e.spevak@tcsbank.ru>
2022-11-08 12:38:31 +0500

    created initial models

--- base/models.py
+++ base/models.py
@@ -1,3 +1,59 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+
+    name = models.CharField(max_length = 20, null = True)
+    email = models.EmailField(unique = True, null = True)
+    bio = models.TextField(null = True, blank = True)
+
+    avatar = models.ImageField(null = True, default = "avatar.svg", blank = True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+
+class Category(models.Model):
+    name = models.CharField(max_length = 50)
+
+    class Meta:
+        ordering = ['name']
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name[:50]
+
+
+
+class Product(models.Model):
+
+    name = models.CharField(max_length = 100, unique = True)
+    description = models.TextField(null = True, blank = True)
+    image = models.ImageField(default = 'empty.png', null = True, blank = True)
+    availiable = models.BooleanField(default = True)
+    price = models.FloatField()
+    created = models.DateTimeField(auto_now_add = True)
+    updated = models.DateTimeField(auto_now = True)
+    discount = models.FloatField(null = True)
+    category = models.ManyToManyField(Category)
+
+    class Meta:
+        ordering = ['-updated', '-created']
+
+    def __str__(self):
+        return self.name
+
+class Comment(models.Model):
+
+    body = models.TextField()
+    author = models.ForeignKey(User, on_delete = models.CASCADE)
+    product = models.ForeignKey(Product, on_delete = models.CASCADE)
+    created = models.DateTimeField(auto_now_add = True)
+
+    class Meta:
+        ordering = ['-created']
+
+    def __str__(self):
+        return self.body
 
 
--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -101,9 +101,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
+MEDIA_ROOT = BASE_DIR / 'static/images'
+MEDIA_URL = '/images/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+AUTH_USER_MODEL = 'base.User'
